225c5373340abfc2919ec423eb08c34fa3b95e32
unknown <flonoalemil@gmail.com>
2019-10-12 01:28:55 -0500

    mvc con login, alam v1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace crudmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
